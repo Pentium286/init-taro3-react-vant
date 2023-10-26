591e7c06376011bffa637ba0ee922d62ebaa4cf8
sheng <sunwhere0207@gmail.com>
2023-10-26 23:25:46 +0800

    微信蓝牙API添加注释

--- src/components/jbs-weapp/bluetooth-weapp/index.js
+++ src/components/jbs-weapp/bluetooth-weapp/index.js
@@ -27,6 +27,7 @@ Page({
     chs: [],
   },
   openBluetoothAdapter() {
+    // 初始化蓝牙模块。iOS 上开启主机/从机（外围设备）模式时需分别调用一次，并指定对应的 mode。
     wx.openBluetoothAdapter({
       success: (res) => {
         console.log('openBluetoothAdapter success', res);
@@ -34,6 +35,7 @@ Page({
       },
       fail: (res) => {
         if (res.errCode === 10001) {
+          // 监听蓝牙适配器状态变化事件
           wx.onBluetoothAdapterStateChange(function (res) {
             console.log('onBluetoothAdapterStateChange', res);
             if (res.available) {
@@ -45,6 +47,7 @@ Page({
     });
   },
   getBluetoothAdapterState() {
+    // 获取本机蓝牙适配器状态。
     wx.getBluetoothAdapterState({
       success: (res) => {
         console.log('getBluetoothAdapterState', res);
@@ -61,6 +64,8 @@ Page({
       return;
     }
     this._discoveryStarted = true;
+    // 开始搜寻附近的蓝牙外围设备。
+    // 此操作比较耗费系统资源，请在搜索到需要的设备后及时调用 wx.stopBluetoothDevicesDiscovery 停止搜索。
     wx.startBluetoothDevicesDiscovery({
       allowDuplicatesKey: true,
       success: (res) => {
@@ -70,9 +75,11 @@ Page({
     });
   },
   stopBluetoothDevicesDiscovery() {
+    // 停止搜寻附近的蓝牙外围设备。若已经找到需要的蓝牙设备并不需要继续搜索时，建议调用该接口停止蓝牙搜索。
     wx.stopBluetoothDevicesDiscovery();
   },
   onBluetoothDeviceFound() {
+    // 监听搜索到新设备的事件
     wx.onBluetoothDeviceFound((res) => {
       res.devices.forEach(device => {
         if (!device.name && !device.localName) {
@@ -95,6 +102,8 @@ Page({
     const ds = e.currentTarget.dataset;
     const deviceId = ds.deviceId;
     const name = ds.name;
+    // 连接蓝牙低功耗设备。
+    // 若小程序在之前已有搜索过某个蓝牙设备，并成功建立连接，可直接传入之前搜索获取的 deviceId 直接尝试连接该设备，无需再次进行搜索操作。
     wx.createBLEConnection({
       deviceId,
       success: (res) => {
@@ -109,6 +118,7 @@ Page({
     this.stopBluetoothDevicesDiscovery();
   },
   closeBLEConnection() {
+    // 断开与蓝牙低功耗设备的连接。
     wx.closeBLEConnection({
       deviceId: this.data.deviceId
     });
@@ -119,6 +129,7 @@ Page({
     });
   },
   getBLEDeviceServices(deviceId) {
+    // 获取蓝牙低功耗设备所有服务 (service)。
     wx.getBLEDeviceServices({
       deviceId,
       success: (res) => {
@@ -132,6 +143,7 @@ Page({
     });
   },
   getBLEDeviceCharacteristics(deviceId, serviceId) {
+    // 获取蓝牙低功耗设备某个服务中所有特征 (characteristic)。
     wx.getBLEDeviceCharacteristics({
       deviceId,
       serviceId,
@@ -140,6 +152,7 @@ Page({
         for (let i = 0; i < res.characteristics.length; i++) {
           let item = res.characteristics[i];
           if (item.properties.read) {
+            // 读取蓝牙低功耗设备特征值的二进制数据。注意：必须设备的特征支持 read 才可以成功调用。
             wx.readBLECharacteristicValue({
               deviceId,
               serviceId,
@@ -156,6 +169,8 @@ Page({
             this.writeBLECharacteristicValue();
           }
           if (item.properties.notify || item.properties.indicate) {
+            // 启用蓝牙低功耗设备特征值变化时的 notify 功能，订阅特征。注意：必须设备的特征支持 notify 或者 indicate 才可以成功调用。
+            // 另外，必须先启用 wx.notifyBLECharacteristicValueChange 才能监听到设备 characteristicValueChange 事件
             wx.notifyBLECharacteristicValueChange({
               deviceId,
               serviceId,
@@ -202,6 +217,7 @@ Page({
     let buffer = new ArrayBuffer(1);
     let dataView = new DataView(buffer);
     dataView.setUint8(0, Math.random() * 255 | 0);
+    // 向蓝牙低功耗设备特征值中写入二进制数据。注意：必须设备的特征支持 write 才可以成功调用。
     wx.writeBLECharacteristicValue({
       deviceId: this._deviceId,
       serviceId: this._deviceId,
@@ -210,6 +226,7 @@ Page({
     });
   },
   closeBluetoothAdapter() {
+    // 关闭蓝牙模块。调用该方法将断开所有已建立的连接并释放系统资源。建议在使用蓝牙流程后，与 wx.openBluetoothAdapter 成对调用。
     wx.closeBluetoothAdapter();
     this._discoveryStarted = false;
   },
